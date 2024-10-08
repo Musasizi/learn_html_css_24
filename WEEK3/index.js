88c962a8e477c70212e64a7920fc0a8ba9bb2e93
Musasizi Kenneth <24485588+Musasizi@users.noreply.github.com>
2024-10-08 14:16:55 +0300

    🎯intial commit

---
+++ WEEK3/index.js
@@ -0,0 +1,17 @@
+console.log("Hello, word!");
+
+let first_name = "Jane";
+let last_name = "Doe";
+
+const PIE = 3.13423;
+const MAX_SPEED = 4000;
+
+let is_student = false;
+
+
+var school_name = "UCU";
+
+// One line comment
+/*
+MULTILINE COMMENTS
+*/
